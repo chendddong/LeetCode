9afab749158554cec3533b07c906ad1c5587fa0e
Peter <dchen@albany.edu>
2017-07-24 21:39:56 -0400

    Add 141

---
+++ ARRAY_SET/141. Linked List Cycle.java
@@ -0,0 +1,47 @@
+/**
+ * Given a linked list, determine if it has a cycle in it.
+ */
+
+/*
+    Follow up:
+    Can you solve it without using extra space?
+ */
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+//////////////////
+// Two Pointers //
+//////////////////
+
+public class Solution {
+    /* If "slow" and "fast" collide, we then have a cycle */    
+    public boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false;
+        }    
+
+        ListNode slow = head; /* Moves 1 node at a time */
+        ListNode fast = head; /* Moves 2 nodes at a time */
+
+        /* Work both for odd and even number of Nodes; Remember it's && */
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (fast == slow) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
