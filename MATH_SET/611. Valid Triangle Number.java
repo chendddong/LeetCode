0e053cf8e0387fadcd40ef5823cfe92c7091fb7a
Peter <dchen@albany.edu>
2017-08-02 11:02:02 -0400

    Added a more intuitive way

--- MATH_SET/611. Valid Triangle Number.java
+++ MATH_SET/611. Valid Triangle Number.java
@@ -59,3 +59,23 @@ public class Solution {
 
     If the time complexity is not less than nlogn we can just sort it using the generic sort method
 */
+
+/////////////////////////
+// More Intuitive Scan //
+/////////////////////////
+
+public class Solution {
+    public int triangleNumber(int[] nums) {
+        int count = 0;
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            int k = i + 2;
+            for (int j = i + 1; j < nums.length - 1 && nums[i] != 0; j++) {
+                while (k < nums.length && nums[i] + nums[j] > nums[k])
+                    k++;
+                count += k - j - 1;
+            }
+        }
+        return count; /* Walk through and see the happening */
+    }
+}
