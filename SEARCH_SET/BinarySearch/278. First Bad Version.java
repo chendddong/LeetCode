8a9a28b09d28d936b16e14f738df8e77f19ae6c2
Peter <dchen@albany.edu>
2017-08-17 13:24:18 -0400

    Added 278

---
+++ SEARCH_SET/BinarySearch/278. First Bad Version.java
@@ -0,0 +1,62 @@
+/*
+    You are a product manager and currently leading a team to develop a new
+    product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+
+    Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+
+    You are given an API bool isBadVersion(version) which will return whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
+ */
+
+/* 
+    The isBadVersion API is defined in the parent class VersionControl. boolean
+    isBadVersion(int version); 
+ */
+
+/////////////////
+// Linear time //               TLE
+/////////////////
+
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        for (int i = 1; i < n; i++) {
+            if (isBadVersion(i)) {
+                return i;
+            }
+        }
+        return n;
+    }
+}
+
+///////////////////
+// Binary Search //
+///////////////////
+
+class Solution extends VersionControl {
+
+    public int firstBadVersion(int n) {
+        int start = 1;
+        int end = n;
+        
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (isBadVersion(mid)) { /* Generic check */
+                end = mid;
+            } else {
+                start = mid;
+            }
+        }
+        
+        if (isBadVersion(start)) {
+            return start;
+        } 
+        
+        if (isBadVersion(end)) {
+            return end;
+        }
+        
+        return 0;
+    }
+}
