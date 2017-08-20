0230fd912ed68fd8a13e566ea3872f50ff0880cf
Peter <dchen@albany.edu>
2017-08-20 00:27:55 -0400

    Added the inorder traversal version

--- DATA_STUCTURE_SET/TREE_SET/98. Validate Binary Search Tree.java
+++ DATA_STUCTURE_SET/TREE_SET/98. Validate Binary Search Tree.java
@@ -147,3 +147,26 @@ public class Solution {
     The [resultType] of things!
  */
 
+////////////////////////
+// Inorder traversal  //    The goto option
+////////////////////////
+
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        if (root == null) return true;
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode pre = null;
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            /* Solve problem */
+            if(pre != null && root.val <= pre.val) return false;
+            pre = root;
+            root = root.right;
+        }
+        return true;
+    }
+}
